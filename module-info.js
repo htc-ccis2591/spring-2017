57eaa74c6a36ba1e62cee572c16c577fe564a24f
mbMosman <mbmosman@users.noreply.github.com>
2017-01-25 17:53:02 -0600

    updates for mod 3

--- module-info.js
+++ module-info.js
@@ -1,7 +1,8 @@
 CCIS2591 = {
 modules: [
   { course: "CCIS2591", title: "Getting Started", moduleUrl: "http://htc-ccis2591.github.io/spring-2017/modules/intro", sort_order: 1, description: "An introduction to the tools & technology used for this course." },
-  { course: "CCIS2591", title: "Hello JavaScript", moduleUrl: "http://htc-ccis2591.github.io/spring-2017/modules/hello-js", sort_order: 2, description: "An introduction to basic JavaScript syntax." }
+  { course: "CCIS2591", title: "Hello JavaScript", moduleUrl: "http://htc-ccis2591.github.io/spring-2017/modules/hello-js", sort_order: 2, description: "An introduction to basic JavaScript syntax." },
+  { course: "CCIS2591", title: "Functions & Scope", moduleUrl: "http://htc-ccis2591.github.io/spring-2017/modules/functions-scope", sort_order: 3, description: "An introduction to the use of functions and scope in JavaScript." }
 ],
  prerequisites: [
 ]
